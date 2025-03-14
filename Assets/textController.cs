f566ae5723a54766526e4e7478461db3728678e7
Perka <lemergabriel55@gmail.com>
2025-03-14 18:43:37 +0100

    [ADD] Dialogue improvement - HOT RELOAD

--- Assets/textController.cs
+++ Assets/textController.cs
@@ -18,12 +18,22 @@ public class textController : MonoBehaviour
     [SerializeField] private TextMeshPro dialogueText;
     [Foldout("OTHERS")]
     [SerializeField] private TypewriterByCharacter typewriter;
+    [Foldout("OTHERS")]
+    [SerializeField] private GameObject _skipIndicator;
     
     private bool _canSkip;
 
     
     private int _currentLine;
 
+    private bool _dialogueStarted = false;
+    private bool _dialogueEnded = false;
+
+
+    private void Start()
+    {
+        SetMeshskip(false);
+    }
 
     private void Update()
     {
@@ -35,9 +45,16 @@ public class textController : MonoBehaviour
 
     public void EndLine()
     {
+        if(_dialogueEnded) return;
+        if (!_dialogueStarted)
+        {
+            _dialogueStarted = true;
+                            NewLine();
+                            return;
+        }
         if (_canSkip)
         {
-            _canSkip = false;
+            SetSkip(false);
             _currentLine++;
             if (_currentLine < dialogue.dialogueLines.Count)
             {
@@ -45,18 +62,21 @@ public class textController : MonoBehaviour
             }
             else
             {
-                EndDialogue();
+               EndDialogue();
             }
         }
         else
         {
-            _canSkip = true;
+            print("Skipping");
+            SetSkip(true);
             typewriter.SkipTypewriter();
         }
     }
     
     public void EndDialogue()
     {
+        _dialogueEnded = true;  
+        dialogueText.text = "";
     }
     
     public void NewLine()
@@ -67,5 +87,11 @@ public class textController : MonoBehaviour
     public void SetSkip(bool value)
     {
         _canSkip = value;
+        SetMeshskip(value);
+    }
+    
+    private void SetMeshskip(bool value)
+    {
+        _skipIndicator.SetActive(value);
     }
 }
