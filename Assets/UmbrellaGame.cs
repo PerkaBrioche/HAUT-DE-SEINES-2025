188c78df4222ddf50abff2dbf8b8dfd464f4122d
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-03-27 12:00:08 +0100

    Update

--- Assets/UmbrellaGame.cs
+++ Assets/UmbrellaGame.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,38 +6,33 @@ public class UmbrellaGame : MonoBehaviour
 {
     [SerializeField] private UnityEvent OnFinished;
     [SerializeField] private float speed;
-    [SerializeField] private Vector2 constrain;
     [SerializeField] private Transform umbrella;
+    [SerializeField] private TextMeshProUGUI tmp;
+    public string text;
     float startX = 0f;
-
-    private void OnDrawGizmos()
-    {
-        if(umbrella != null)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawSphere(umbrella.position - Vector3.right * constrain.x, 0.5f);
-            Gizmos.DrawSphere(umbrella.position + Vector3.right * constrain.y, 0.5f);
-        }
-    }
 
     private void Start()
     {
         startX = umbrella.position.x;
+        tmp.text = text;
+        tmp.ForceMeshUpdate();
+        textWidth = tmp.GetRenderedValues().x;
     }
 
     private bool hasFinished = false;
+    private float textWidth;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && !hasFinished)
         {
             umbrella.position += Vector3.right * speed * Time.deltaTime;
 
             Vector3 pos = umbrella.position;
-            pos.x = Mathf.Clamp(pos.x, startX - constrain.x, startX + constrain.y);
+            pos.x = Mathf.Clamp(pos.x, startX, startX + textWidth);
             umbrella.position = pos;
 
-            if (umbrella.position.x >= constrain.y && !hasFinished)
+            if (umbrella.position.x >= startX + textWidth)
             {
                 OnFinished.Invoke();
                 hasFinished = true;
